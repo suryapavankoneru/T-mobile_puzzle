8e6f9ea9521ce4b53f8f5537201841150b4d72e9
suryapavankoneru <suryakoneru@mdits.co.in>
2020-10-08 12:04:01 +0530

    First commit

---
+++ libs/api/books/src/lib/books.controller.ts
@@ -0,0 +1,23 @@
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+
+@Controller()
+export class BooksController {
+  constructor(private readonly books: BooksService) {}
+
+ // @Get('/books/search')
+  you should have a method here
+  async searchBooks(@Query('q') term) {
+    try {
+      return await this.books.search(term);
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+  }
+}
